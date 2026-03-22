3ff6fa6f8d829fa7fb944ccc4ffaaa69f8cad956
Paul Mullins <paulmullins@protonmail.com>
2026-03-21 20:31:08 -0400

    Got the site build n stuff

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+node ./scripts/set_key_value_in_path.js \
+  -i ./collective-memex/ \
+  -t share \
+  -v true
+
+
 #!/bin/bash
 source .env
 echo 'build_path'
@@ -9,5 +15,15 @@ node raw_to_sqlite.js \
     -dbf $build_path/pkm.sqlite \
     -i $pkm_in_path \
     -o $build_path \
-    -oi 5
+    # -oi 5
 
+# Build the markdown
+cd $build_path
+rm -rf docs/
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install mkdocs
+python3 -m pip install mkdocs-material
+python3 -m pip install mkdocs-backlinks
+python3 -m pip install material-plausible-plugin
+python3 -m mkdocs build -v
